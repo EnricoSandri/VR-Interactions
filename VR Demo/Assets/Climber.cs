43687e653b3c6465bed476749899be08338dd0a4
Enrico <sandri.enrico86@gmail.com>
2021-10-14 13:55:37 +0100

    Climbing

---
+++ VR Demo/Assets/Climber.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class Climber : MonoBehaviour
+{
+    private CharacterController character;
+    public static XRController climbingHand;
+    private ContinuousMovement continuosMovement;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        continuosMovement = GetComponent<ContinuousMovement>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (climbingHand)
+        {
+            continuosMovement.enabled = false;
+            Climb();
+        }
+        else
+        {
+            continuosMovement.enabled = true;
+        }
+    }
+
+    void Climb()
+    {
+        InputDevices.GetDeviceAtXRNode(climbingHand.controllerNode)
+            .TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
+
+        character.Move(transform.rotation * -velocity*Time.fixedDeltaTime);
+    }
+}
